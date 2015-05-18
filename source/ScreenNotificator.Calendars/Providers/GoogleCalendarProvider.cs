aa89cfccdb2ae639eb832b6015de83f9c0bbceee
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-05-18 23:47:13 +0300

    Add separate project for calendar data providers.

---
+++ source/ScreenNotificator.Calendars/Providers/GoogleCalendarProvider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScreenNotificator.Calendars.Providers
+{
+	public class GoogleCalendarProvider : ICalendarProvider
+	{
+		public IEnumerable<string> GetCalendarList()
+		{
+			throw new NotImplementedException();
+		}
+
+		public IEnumerable<string> GetEventsFromCalendar(IEnumerable<string> calendarIDs, DateTime startDate, DateTime? endDate)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ source/ScreenNotificator.Calendars/Providers/ICalendarProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ScreenNotificator.Calendars.Providers
+{
+	public interface ICalendarProvider
+	{
+		IEnumerable<string> GetCalendarList();
+		IEnumerable<string> GetEventsFromCalendar(IEnumerable<string> calendarIDs, DateTime startDate, DateTime? endDate);
+	}
+}
