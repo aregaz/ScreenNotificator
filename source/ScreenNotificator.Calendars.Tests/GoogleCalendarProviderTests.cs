bcb2862a34289b585ce0f793acc76e05a07a78d0
Illia Ratkevych <illia.ratkevych@gmail.com>
2015-06-24 21:52:20 +0300

    intermediate commit.

---
+++ source/ScreenNotificator.Calendars.Tests/GoogleCalendarProviderTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ScreenNotificator.Calendars.Providers;
+
+namespace ScreenNotificator.Calendars.Tests
+{
+	[TestClass]
+	public class GoogleCalendarProviderTests
+	{
+		[TestMethod]
+		public void Do__Success()
+		{
+			var provider = new GoogleCalendarProvider();
+			provider.Do();
+			
+			Assert.IsTrue(true);
+		}
+	}
+}
--- source/ScreenNotificator.Calendars/Providers/GoogleCalendarProvider.cs
+++ source/ScreenNotificator.Calendars/Providers/GoogleCalendarProvider.cs
@@ -1,10 +1,22 @@
-﻿using System;
+﻿using System.Diagnostics;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Threading;
 
 namespace ScreenNotificator.Calendars.Providers
 {
 	public class GoogleCalendarProvider : ICalendarProvider
 	{
+		static readonly string[] Scopes = { CalendarService.Scope.CalendarReadonly };
+		private const string ApplicationName = "ScreenNotificator";
+
+
 		public IEnumerable<string> GetCalendarList()
 		{
 			throw new NotImplementedException();
@@ -14,5 +26,62 @@ namespace ScreenNotificator.Calendars.Providers
 		{
 			throw new NotImplementedException();
 		}
+
+		public void Do()
+		{
+			UserCredential credential;
+
+			using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+			{
+				var credPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+				credPath = Path.Combine(credPath, ".credentials");
+
+				credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+					GoogleClientSecrets.Load(stream).Secrets,
+					Scopes,
+					"user",
+					CancellationToken.None,
+					new FileDataStore(credPath, true)).Result;
+
+				Debug.WriteLine("Credential file saved to: " + credPath);
+			}
+
+			// Create Calendar Service.
+			var service = new CalendarService(new BaseClientService.Initializer()
+			{
+				HttpClientInitializer = credential,
+				ApplicationName = ApplicationName,
+			});
+
+			// Define parameters of request.
+			var request = service.Events.List("primary");
+			request.TimeMin = DateTime.Now;
+			request.ShowDeleted = false;
+			request.SingleEvents = true;
+			request.MaxResults = 10;
+			request.OrderBy = EventsResource.ListRequest.OrderByEnum.StartTime;
+
+			Debug.WriteLine("Upcoming events:");
+
+			var events = request.Execute();
+			if (events.Items.Count > 0)
+			{
+				foreach (var eventItem in events.Items)
+				{
+					var when = eventItem.Start.DateTime.ToString();
+					if (string.IsNullOrEmpty(when))
+					{
+						when = eventItem.Start.Date;
+					}
+
+					Debug.WriteLine("{0} ({1})", eventItem.Summary, when);
+				}
+			}
+			else
+			{
+				Debug.WriteLine("No upcoming events found.");
+			}
+		}
+
 	}
 }
